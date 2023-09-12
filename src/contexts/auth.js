180aff69c705b8e44e04c06d4b9825c8abf56f45
rezende130216 <ra-21157141-2@alunos.unicesumar.edu.br>
2023-09-12 15:01:49 -0300

    Alterações no LocalStorage

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -3,6 +3,7 @@ import { auth, db } from "../services/Banco/firebaseConnection";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
@@ -13,12 +14,25 @@ export const AuthContext = createContext({});
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loadingAuth, setLoadingAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    async function loadUser() {
+      const storageUser = localStorage.getItem("SistemaUser");
+      if (storageUser) {
+        setUser(JSON.parse(storageUser));
+        setLoading(false);
+      }
+      setLoading(false);
+    }
+    loadUser();
+  }, []);
 
   async function signIn(email, password) {
     setLoadingAuth(true);
-    await signInWithEmailAndPassword(auth, email, password).then(
-      async (value) => {
+    await signInWithEmailAndPassword(auth, email, password)
+      .then(async (value) => {
         let uid = value.user.uid;
         const docRef = doc(db, "users", uid);
         const docSnap = await getDoc(docRef);
@@ -28,17 +42,18 @@ function AuthProvider({ children }) {
           name: docSnap.data().name,
           email: value.user.email,
           avatarUrl: docSnap.data().avatarUrl,
-        }
+        };
         setUser(data);
+        storageUser(data);
         setLoadingAuth(false);
         toast.success("Welcome to the system");
         navigate("/dashboard");
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         console.log(error);
         setLoadingAuth(false);
         toast.error("Error logging in");
-        });
+      });
   }
 
   async function signUp(email, password, name) {
@@ -58,6 +73,7 @@ function AuthProvider({ children }) {
             avatarUrl: null,
           };
           setUser(data);
+          storageUser(data);
           setLoadingAuth(false);
           toast.success("Welcome to the system");
           navigate("/dashboard");
@@ -68,6 +84,16 @@ function AuthProvider({ children }) {
         setLoadingAuth(false);
       });
   }
+  function storageUser(data) {
+    localStorage.setItem("SistemaUser", JSON.stringify(data));
+  }
+
+  async function logout() {
+    await signOut(auth);
+    localStorage.removeItem("SistemaUser");
+    setUser(null);
+    navigate("/");
+  }
 
   return (
     <AuthContext.Provider
@@ -76,7 +102,9 @@ function AuthProvider({ children }) {
         user,
         signIn,
         signUp,
+        logout,
         loadingAuth,
+        loading
       }}
     >
       {children}
--- src/routes/Private.js
+++ src/routes/Private.js
@@ -3,8 +3,12 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from "../contexts/auth";
 
 export default function Private({ children }) {
-    const { signed } = useContext(AuthContext);
+    const { signed, loading } = useContext(AuthContext);
 
+    if (loading) {
+        return <div></div>;
+    }
+    
     if (!signed) {
         return <Navigate to="/" />;
     }
