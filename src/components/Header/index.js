05b6977830ebcb39ada7810b02edf8f70f902aba
Guilherme Rezende Silvestre <106764372+rezende130216@users.noreply.github.com>
2023-10-31 11:14:58 -0300

    Revert "Alterações teste"

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,7 @@
-import logo from  '../../assets/logo.jpg'
-import {Link} from 'react-router-dom'
-import {useContext} from 'react'
-import {AuthContext} from "../../contexts/auth";
-
 export default function Header() {
-
-    const {user} = useContext(AuthContext);
     return (
-        <div>
-            <div>
-                <img src={user.avatarUrl === null ? logo : user.avatarUrl} alt="photo user"/>
-            </div>
-        </div>
+        <header>
+            <h1>Header</h1>
+        </header>
     )
 }
