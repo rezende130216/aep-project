2071f963907e7a3f35ed1939e3af68e6dcb9b3ee
rezende130216 <ra-21157141-2@alunos.unicesumar.edu.br>
2023-11-07 21:26:58 -0300

    teste

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,32 @@
-import {useContext} from 'react'
-import logo from '../../assets/logo.jpg'
-import {Link} from 'react-router-dom'
-import {AuthContext} from '../../contexts/auth'
-import {FiHome,FiUser,FiFlag} from 'react-icons/fi'
-import './header.css'
+import { useContext } from "react";
+import logo from "../../assets/logo.jpg";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/auth";
+import { FiHome, FiUser, FiFlag } from "react-icons/fi";
+import "./header.css";
 
 export default function Header() {
-    const {user} = useContext(AuthContext);
-    return (
-        <div className='sidebar'>
-            <div>
-                <img src={user.avatarUrl === null ? logo : user.avatarUrl} alt="photo user" />
-            </div>
-            <Link to="/prompts">
-                <FiHome color="#FFF" size={24} />
-                Prompts
-            </Link>
-            <Link to="/dashboard">
-                <FiFlag color="#FFF" size={24} />
-                Saved
-            </Link>
-            <Link to="/profile">
-                <FiUser color="#FFF" size={24} />
-                Profile
-            </Link>
-        </div>
-    )
+  const { user } = useContext(AuthContext);
+  return (
+    <div className="sidebar">
+      <div>
+        <img
+          src={avatarUrl || logo}
+          alt={user.name ? `Avatar de ${user.name}` : "Avatar"}
+        />
+      </div>
+      <Link to="/prompts">
+        <FiHome color="#FFF" size={24} />
+        Prompts
+      </Link>
+      <Link to="/dashboard">
+        <FiFlag color="#FFF" size={24} />
+        Saved
+      </Link>
+      <Link to="/profile">
+        <FiUser color="#FFF" size={24} />
+        Profile
+      </Link>
+    </div>
+  );
 }
