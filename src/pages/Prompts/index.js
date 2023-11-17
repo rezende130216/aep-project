9bd08a7677e56a88bcdf9ae021cc931555a386de
devCurti <joaocurti2002@gmail.com>
2023-11-17 14:52:44 -0300

    Integração com API

--- src/pages/Prompts/index.js
+++ src/pages/Prompts/index.js
@@ -2,7 +2,39 @@ import Title from "../../components/Title";
 import Header from "../../components/Header";
 import { FiSettings } from "react-icons/fi";
 import "./prompts.css";
+import { useState } from "react";
+import OpenAI from 'openai';
+
+
+const openai = new OpenAI({
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true
+});
+
 export default function Prompts() {
+
+  const [prompt, setPrompt] = useState("");
+  const [promptQuestion, setPromptQuestion] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handleSubmit(e) {
+    setIsLoading(true);
+  console.log(promptQuestion);
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{'role': 'user', 'content': promptQuestion}]
+  });
+
+  const generatedText = response['choices'][0]['message']['content'];
+
+  await setPrompt((prevPrompt) =>  prevPrompt + '\n\n' + generatedText );
+
+  console.log(generatedText.toLowerCase);
+
+  setIsLoading(false);
+  }
+
   return (
     <div>
       <Header />
@@ -11,16 +43,14 @@ export default function Prompts() {
           <FiSettings size={25} />
         </Title>
         <div className="container-00">
-            <span>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </span>
-            <span>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </span>
+          <div className="responseDiv" style={{ whiteSpace: 'pre-line' }}>
+            {prompt ? "" : "Pegunte a IA!"}
+            {isLoading ? <div>Generating...</div> : prompt}
+          </div>
         </div>
         <div className="container-01">
-            <textarea rows={10} type="text"/>
-            <button>Send</button>
+          <textarea rows={10} type="text" onChange={e => setPromptQuestion(e.target.value)} />
+          <button onClick={handleSubmit}>Send</button>
         </div>
       </div>
     </div>
